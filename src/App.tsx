c0a88549a6843ad01c82757bfbea191602643409
Guilherme Caçador <gxavier@esapiens.com.br>
2021-12-04 20:10:39 -0300

    Code sliting

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Side from './pages/Side/Side'
 import { ThemeStorage } from './ThemeContext'
 import './variables.scss'
+
+const Home = lazy(() => import('./pages/Home/Home'))
+const Side = lazy(() => import('./pages/Side/Side'))
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeStorage>
         <main>
-          <Routes>
-            <Route path="/*" element={<Home />} />
-            <Route path="/side" element={<Side />} />
-          </Routes>
+          <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
+              <Route path="/*" element={<Home />} />
+              <Route path="/side" element={<Side />} />
+            </Routes>
+          </Suspense>
         </main>
       </ThemeStorage>
     </BrowserRouter>
