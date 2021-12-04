76f0e91bc849103f2d627085fbb15aba6a5a85e1
Guilherme Caçador <gxavier@esapiens.com.br>
2021-12-04 19:36:41 -0300

    Include back arrow

--- src/pages/Side/Side.tsx
+++ src/pages/Side/Side.tsx
@@ -3,14 +3,19 @@ import Button from '../../components/Button/Button'
 import Image from '../../components/Image/Image'
 import { ThemeContext } from '../../ThemeContext'
 import styles from './Side.module.scss'
+import backArrow from '../../static/icons/back-arrow.svg'
 
 function Side() {
-  const { theme, forceSide, getForceSide, loading } =
+  const { theme, forceSide, getForceSide, loading, setTheme } =
     React.useContext(ThemeContext)
 
   if (!theme || !forceSide) return null
   return (
     <section className={`${theme || ''} ${styles.side}`}>
+      <button className={styles.back} onClick={() => setTheme(null)}>
+        <img className={styles[theme]} src={backArrow} alt="Arrow back" />
+        <span className={styles.backText}>back</span>
+      </button>
       <Button
         onClick={() => getForceSide()}
         disabled={loading}
